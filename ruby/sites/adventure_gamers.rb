dfa73bf885378f322dc1558114577895c648a5c8
Peter <peter.rootham@alofmethbin.com>
2021-09-05 16:12:51 +0100

    Remove debug code

--- ruby/sites/adventure_gamers.rb
+++ ruby/sites/adventure_gamers.rb
@@ -1,9 +1,6 @@
 class AdventureGamers
 	def find( scanner)
 		scanner.twitter_feed_links( 'adventuregamers') do |text, link|
-			if /Root Letter/ =~ text
-				p [text, link]
-			end
 			if /^https:\/\/adventuregamers\.com\/articles\/view\/.*$/ =~ link
 				scanner.add_link( '', link)
 			else
