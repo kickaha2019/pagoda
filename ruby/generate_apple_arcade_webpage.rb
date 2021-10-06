e39737e4b0f6f6a561b6dc4c704ea48cefecf4cb
Peter <peter.rootham@alofmethbin.com>
2021-10-06 10:04:53 +0100

    More Apple like colour scheme and title has N out N in it

--- ruby/generate_apple_arcade_webpage.rb
+++ ruby/generate_apple_arcade_webpage.rb
@@ -27,7 +27,7 @@ FILTER1
 <SPAN ID="#{code}#{@option}" 
 ONCLICK="flip( '#{code}#{@option}', #{attribute.downcase}, #{index});" 
 CLASS="option">
-#{values[index]}
+#{values[index].upcase}
 </SPAN>
 FILTER2
       @option += 1
@@ -42,7 +42,7 @@ FILTER3
 <DIV CLASS="filters">
 <DIV ID="filters_hidden">
 <DIV CLASS="triangle_right" ONCLICK="hide('filters_hidden');show('filters_shown');"></DIV>
-<SPAN ID="filter_status">Filters: all games listed</SPAN>
+<SPAN ID="filter_status">Filters</SPAN>
 </DIV>
 <DIV ID="filters_shown">
 <DIV CLASS="triangle_down" ONCLICK="hide('filters_shown');show('filters_hidden');"></DIV>
@@ -74,7 +74,7 @@ BODY_LIST
     io.puts <<"TITLE"
 <DIV CLASS="header">
 <SPAN CLASS="note">Generated #{today}</SPAN>
-<SPAN CLASS="title">List of Apple Arcade games</SPAN>
+<SPAN ID="title" CLASS="title">List of Apple Arcade games</SPAN>
 <A CLASS="note" HREF="dummy.html">About this page</A>
 </DIV>
 TITLE
@@ -88,6 +88,7 @@ TITLE
   end
 
   def check_name( arcade, ember_json)
+    return unless ember_json['name']
     if arcade.name != ember_json['name'].gsub( '®', '&reg;').gsub( '™', '&trade;')
       error( "Name mismatch for #{arcade.name} / #{ember_json['name']}")
     end
@@ -99,6 +100,11 @@ TITLE
 
   def declare_values( name, values, io)
     io.puts "var #{name} = [#{values.collect {|v| "\"#{v}\""}.join(',')}];"
+  end
+
+  def e(text)
+    return '' if text.nil?
+    text.gsub( "'", "&#39;")
   end
 
   def error( msg)
@@ -120,8 +126,12 @@ TITLE
       apple_page = http_get_cached( @cache, arcade.url, 45 * 24 * 60 * 60)
       extract_ember_json( arcade.url, apple_page)
     else
-      {'name' => arcade.name}
+      {}
     end
+  end
+
+  def get_platforms( arcade, ember_json)
+    ember_json['operatingSystem']
   end
 
   def html_footer( io)
@@ -136,40 +146,51 @@ FOOTER
 <HTML>
 <HEAD>
 <STYLE>
+body {background-color: #242424}
 .header {display:flex; justify-content: space-between;}
-.note {font-size: 25px;}
-.title {font-size: 30px; font-weight: bold;}
+.note {font-size: 25px; color: #BFBFBF;}
+.title {font-size: 30px; font-weight: bold; color: #BFBFBF;}
 #list {
   display: flex;
   justify-content: center;
 }
+
 table {
-  font-size: 25px;
-  border-collapse: collapse;
+  font-size: 20px;
+  color: white;
 }
 
-td, th {padding: 5px}
-#list td, #list th {border-left: 1px solid black}
-#list th {border-bottom: 1px solid black}
+#list table {
+  font-size: 25px;
+  border-collapse: collapse; 
+}
+
+td, th {padding: 8px}
+table a {color: white}
+table a:visited {color: white}
+
+#list td, #list th {border-left: 1px solid white}
+#list th {border-bottom: 1px solid white}
 #list td:nth-child(1), #list th:nth-child(1) {border-left: 0px}
 
-#list tr:nth-child(even) td {background: cyan}
-#list tr:nth-child(odd)  td {background: lime}
+#list tr:nth-child(even) td {background: #1C1C1C}
+#list tr:nth-child(odd)  td {background: #3D3D3D}
 
-.option {font-size: 30px;
+.option {font-size: 15px;
          -moz-user-select: none;
          -webkit-user-select: none;
          -ms-user-select: none;
          user-select: none;
-         color: black;
-         background-color: yellow;
-         border: 1px solid black;
-         border-radius: 5px;
+         color: blue;
+         background-color: white;
+         border-radius: 8px;
          padding: 3px;
+         padding-left: 15px;
+         padding-right: 15px;
          margin: 5px;
 }
 .options {display: flex; flex-wrap: wrap;}
-.filters {font-size: 25px; margin-top: 10px}
+.filters {font-size: 25px; margin-top: 5px; color: white}
 #filters_hidden {
   display: flex;
 }
@@ -177,6 +198,7 @@ td, th {padding: 5px}
   display: none;
 }
 #filters_shown th:nth-child(1) {text-align: left;}
+#filter_status {font-weight: bold; margin-top: 16px;}
 
 .triangle_down {
   display: inline-block;
@@ -184,8 +206,8 @@ td, th {padding: 5px}
 	height: 0;
 	border-left: 8px solid transparent;
 	border-right: 8px solid transparent;
-	border-top: 16px solid #555;
-  margin-top: 7px;
+	border-top: 16px solid cyan;
+  margin-top: 19px;
   margin-right: 10px;
 }
 .triangle_right {
@@ -193,33 +215,24 @@ td, th {padding: 5px}
 	width: 0;
 	height: 0;
 	border-top: 8px solid transparent;
-	border-left: 16px solid #555;
+	border-left: 16px solid cyan;
 	border-bottom: 8px solid transparent;
-  margin-top: 7px;
+  margin-top: 20px;
   margin-right: 10px;
 }
 .filter_control {font-size: 40px; cursor: pointer}
 </STYLE>
 <SCRIPT>
 var inactive = new Object();
-var activeCount = #{options_count};
 
 function flip( oid, flags, index) {
   s1 = document.getElementById( oid).style;
   if (flags[index] == 1) {
     flags[index] = 0;
-    activeCount -= 1;
-    s1.backgroundColor = 'lightgray';
+    s1.backgroundColor = '#888';
   } else {
     flags[index] = 1;
-    s1.backgroundColor = 'yellow';
-    activeCount += 1;
-  }
-  fs = document.getElementById( 'filter_status');
-  if (activeCount < #{number_of_filters}) {
-    fs.innerHTML = 'Filters: some games not shown';
-  } else {
-    fs.innerHTML = 'Filters: all games listed';
+    s1.backgroundColor = 'white';
   }
   list_all();
 }
@@ -254,8 +267,16 @@ HEADER2
   def list_all_function( io)
     io.puts <<"FUNCTION_ALL1"
 var listing = [];
+var total = 0;
+var count = 0;
 function list_all() {
-  listing = ['<TABLE><TR><TH>Name</TH><TH>Dexterity</TH><TH>Difficulty</TH><TH>Depth</TH><TH>Genre</TH></TR>'];
+  listing = ['<TABLE><TR><TH>Name</TH>',
+             '<TH>Dexterity</TH>',
+             '<TH>Difficulty</TH>',
+             '<TH>Depth</TH>',
+             '<TH>Genre</TH></TR>'];
+  total = 0;
+  count = 0;
 FUNCTION_ALL1
 
     @games.each do |arcade|
@@ -269,7 +290,7 @@ FUNCTION_ALL1
         next if dex.nil? || dif.nil? || dep.nil? || gen.nil?
 
         io.puts <<"FUNCTION_ALL2"
-      list_one( '#{arcade.name.gsub( "'", "&#39;")}', '#{arcade.url}', #{dex}, #{dif}, #{dep}, #{gen});
+      list_one( '#{e(arcade.name)}', '#{arcade.url}', #{dex}, #{dif}, #{dep}, #{gen});
 FUNCTION_ALL2
       rescue Exception => bang
         error( "#{bang.message} for #{arcade.name}")
@@ -279,6 +300,7 @@ FUNCTION_ALL2
     io.puts <<"FUNCTION_ALL3"
   listing.push( '</TABLE>');  
   document.getElementById( 'list').innerHTML = listing.join('');
+  document.getElementById( 'title').innerHTML = count + ' out of ' + total + ' Apple Arcade games';
 }
 FUNCTION_ALL3
   end
@@ -286,25 +308,23 @@ FUNCTION_ALL3
   def list_one_function( io)
     io.puts <<"FUNCTION1"
 function list_one( name, url, dex, dif, dep, gen) {
+  total += 1;
   if ((dex > 0) && (!  dexterity[dex-1])) {return;}
   if ((dif > 0) && (! difficulty[dif-1])) {return;}
   if (! depth[dep]) {return;}
   if (! genre[gen]) {return;}
-  listing.push( '<TR><TD><A HREF="' + url + '">' + name + '</A></TD>');
+  listing.push( '<TR><TD><A TARGET=“_blank” REL=“nofollow” HREF="' + url + '">' + name + '</A></TD>');
   listing.push( '<TD>' + dexterities[dex] + '</TD>');
   listing.push( '<TD>' + difficulties[dif] + '</TD>');
   listing.push( '<TD>' + depths[dep] + '</TD>');
   listing.push( '<TD>' + genres[gen] + '</TD></TR>');
+  count += 1;
 }
 FUNCTION1
   end
 
   def number_of_filters
     @difficulties.size + @dexterities.size + @depths.size + @genres.size
-  end
-
-  def options_count
-    9 + @genres.size
   end
 
   def today
